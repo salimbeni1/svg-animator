fd404a37c319192e7ad2b2fe3b1043be228376d4
salimbeni1 <etienne.salimbeni@epfl.ch>
2022-01-11 18:28:39 +0100

    sketch of front page snake head animation

--- web-app/pages/index.js
+++ web-app/pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import styles from '../styles/Home.module.scss'
+import { getSnakeHead } from '../svgs/snake/snakeHead'
 
 export default function Home() {
   return (
@@ -13,6 +14,15 @@ export default function Home() {
       <Link href='app'>
       <a className={styles.btn}> START ANIMATE NOW </a>
       </Link>
+
+    </div>
+
+
+    <div className={styles.snakectn}>
+
+      <div className={styles.snakeHead} >{getSnakeHead() }</div>
+
+     
 
     </div>
     
